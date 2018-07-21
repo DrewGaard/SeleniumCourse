5d505767dd4e681fb5afd3f5d4369ad2def1c31c
Drew <d_overgaard1@u.pacific.edu>
2018-07-21 16:29:03 -0700

    Commiting Selenium Project changes

---
+++ src/FirstExampleUsingSelenium.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstExampleUsingSelenium {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Drew\\Downloads\\geckodriver-v0.21.0-win64\\geckodriver.exe");
+		
+		WebDriver driver = new FirefoxDriver();
+		
+		driver.get("http://google.com");
+	}
+
+}
